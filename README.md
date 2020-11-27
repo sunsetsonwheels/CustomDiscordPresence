ef6d1b43baa5517c996151df739d5e1089a43f1c
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2020-11-27 23:18:59 +0800

    ✨Brand spanking new rewrite!

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # CustomDiscordPresence
- Set your own Discord Rich Presence
+
+![](presence_example.png)
+
+> Ever wanted to set your own rich presence?
+
+> Want to stay swag and cool among the other lame rich presences?
+
+> Always wanted to start a conversation your crush on Discord?
+
+CustomDiscordPresence is a simple Python script that helps you do exactly that! (~~except the crush part but let's just keep it there for the sake of attractiveness~~).
+
+It's simple to setup too, so you'd be up and flexing your brand spanking new, special and fragilicious Rich Presence in no time.
+
+Not to mention we should work anywhere the Discord Desktop app works properly too! 
+
+## Setting up
+0. Set up a Discord Developer Application
+
+- Go to Discord's Developer Portal: https://discord.com/developers/applications
+- Create a new application (the name will be the one displayed in the Rich Presence).
+- Add some image assets if you want to (highly recommended for swag).
+
+
+
+1. Download a release
+
+Get the latest release from [here](releases) for your appropiate platform, or get a copy of the source code to run yourself.
+
+Don't forget to extract it!
+
+2. Get your config file filled up
+
+Make a copy of `cdrpcfg-template.json` and name it `cdrpcfg.json`
+
+Fill up that new file with the required fields, using the [configuration file layout](#configuration-file-layout) as your helping hand. Optional fields are marked with `null`.
+
+3. Start CustomRichPresence
+
+Double click on the CustomRichPresence app to start displaying your Rich Presence.
+
+Ctrl+C will stop the Rich Presence while the app is in focus.
+
+You may be warned to trust the app. Don't worry about it, I won't put anything suspicious on your device, look at the code yourself. So trust it!
+
+## Configuration file layout
+- `client_id`:  Your app's client ID. Get it from the Discord Developer Portal.
+- `state`:
+    - `state`: The state. (this is displayed on the second line, see above screenshot)
+    - `details`: The state's details. (this is displayed on the first line, see above screenshot)
+    - `timestamps`:
+        - `start`: the start timestamp in Unix time epoch.
+        - `end`: the ending timestamp in Unix time epoch.
+- `images`:
+    - `large`:
+        - `name`: The name of the large image (set it to the one you have in your Discord Developer Portal).
+        - `tooltip`: The text that is shown when the large image is hovered.
+    - `small`:
+        - `name`: The name of the small image (set it to the one you have in your Discord Developer Portal).
+        - `tooltip`: The text that is shown when the small image is hovered.
--- cdrp.py
+++ cdrp.py
@@ -1,120 +1,49 @@
-import tkinter as tk
-import tkinter.ttk as ttk
+__author__ = 'jkelol111'
+__copyright__ = 'Copyright (C) 2020-present jkelol111.'
+__license__ = 'Public Domain'
+__version__ = '1.0.0'
+
 import os
+import pathlib
+import json
 import pypresence
 import time
-import threading
+import colorama
 
+colorama.init(autoreset=True)
 
-class DiscordPresence:
-    def __init__(self):
-        self.DISCORD_CLIENT_ID = "621589019129544718"
-        self.rpc_connected = False
-        self.presence_enabled = False
-        self.presence_stopped = True
-        self.rpc = pypresence.Presence(self.DISCORD_CLIENT_ID)
-        self.ac = pypresence.Activity(self.rpc)
+print(f"{colorama.Style.BRIGHT}{colorama.Fore.GREEN}CustomDiscordPresence v{__version__}. {__copyright__}")
+print("Searching for cdrpcfg.json...")
 
-    def set_presence(self, state):
-        if not self.rpc_connected:
-            self.rpc.connect()
-        self.ac.state = state
-        self.presence_enabled = True;
-        while self.presence_enabled:
-            pass
-        self.presence_stopped = True
-        return True
+CONFIG_FILE_PATH = os.path.join(pathlib.Path(__file__).parent, "cdrpcfg.json")
+CONFIG = {}
+if os.path.isfile(CONFIG_FILE_PATH):
+    with open(CONFIG_FILE_PATH, "r") as CONFIG_FILE:
+        CONFIG = json.load(CONFIG_FILE)
+    print(f"{colorama.Fore.GREEN}Found and loaded the config file!")
+else:
+    print(f"{colorama.Fore.RED}Config file not found! Exiting...")
+    exit()
 
-    def cancel_presence(self):
-        self.presence_enabled = False;
-        while not self.presence_stopped:
-            pass
-        return True
+print("Starting the Rich Presence RPC...")
+RPC = pypresence.Presence(CONFIG["client_id"])
+RPC.connect()
 
+RPC.update(state=CONFIG["state"]["state"], 
+           details=CONFIG["state"]["details"], 
+           start=CONFIG["state"]["timestamps"]["start"], 
+           end=CONFIG["state"]["timestamps"]["end"],
+           large_image=CONFIG["images"]["large"]["name"],
+           large_text=CONFIG["images"]["large"]["tooltip"],
+           small_image=CONFIG["images"]["small"]["name"],
+           small_text=CONFIG["images"]["small"]["tooltip"])
 
-class Application:
-    def start_presence(self):
-        pass
-
-    def __init__(self):
-        self.APPLICATION_NAME = "Custom Discord Presence"
-        self.APPLICATION_VERSION = "0.1"
-        self.dp = DiscordPresence()
-        self.root = tk.Tk()
-        self.root.title(self.APPLICATION_NAME)
-        self.root.iconbitmap(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                          "favicon.ico"))
-        self.root["bg"] = "#2c2f33"
-        self.dp_status = tk.StringVar(self.root)
-        self.set_button_label = tk.StringVar(self.root)
-        tk.Label(self.root, 
-                 text=self.APPLICATION_NAME, 
-                 font=("Uni Sans Heavy Italic CAPS", 24), 
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.CENTER).pack(padx=14, 
-                                         pady=6,
-                                         side=tk.TOP)
-        tk.Label(self.root,
-                 text="set your own Discord Rich Presence",
-                 font=("Uni Sans Thin CAPS", 11),
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.CENTER).pack(side=tk.TOP)
-        ttk.Separator(self.root, 
-                      orient='horizontal').pack(side=tk.TOP,
-                                                fill=tk.BOTH)
-        tk.Label(self.root,
-                 text="Client ID",
-                 font=("Uni Sans Thin CAPS", 14),
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.LEFT).pack(side=tk.TOP)
-        ttk.Entry(self.root,
-                  font=("Helvetica Bold", 16),
-                  justify=tk.CENTER).pack(pady=6,
-                                          side=tk.TOP)
-        tk.Label(self.root,
-                 text="Presence",
-                 font=("Uni Sans Thin CAPS", 14),
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.LEFT).pack(side=tk.TOP)
-        ttk.Entry(self.root,
-                  font=("Helvetica Bold", 16),
-                  justify=tk.CENTER).pack(pady=6,
-                                          side=tk.TOP)
-        tk.Button(self.root,
-                  textvariable=self.set_button_label,
-                  font=("Uni Sans Heavy CAPS", 14),
-                  bg="#7289da",
-                  fg="#ffffff").pack(pady=10,
-                                     side=tk.TOP)
-        self.set_button_label.set("Set Rich Presence")
-        ttk.Separator(self.root, 
-                      orient='horizontal').pack(side=tk.TOP,
-                                                fill=tk.BOTH)
-        tk.Label(self.root,
-                 textvariable=self.dp_status,
-                 font=("Uni Sans Heavy CAPS", 11),
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.CENTER).pack(side=tk.TOP)
-        self.dp_status.set("Status: "+"Not set")
-        tk.Label(self.root,
-                 text="{0} v {1}. Made with <) by jkelol111".format(self.APPLICATION_NAME, self.APPLICATION_VERSION),
-                 font=("Uni Sans Thin CAPS", 10),
-                 bg="#2c2f33",
-                 fg="#ffffff",
-                 justify=tk.CENTER).pack(pady=6,
-                                         side=tk.TOP)
-        self.root.mainloop()
-
-if __name__ == "__main__":
-    # test = DiscordPresence()
-    # def testrpc():
-    #     test.set_presence("Custom Presence!")
-    # threading.Thread(target=testrpc).start()
-    # time.sleep(15)
-    # test.cancel_presence()
-    Application()
+try:
+    print(f"{colorama.Fore.GREEN}Rich Presence RPC connected and updated!")
+    while True:
+        time.sleep(15)
+except KeyboardInterrupt:
+    print(f"\n{colorama.Fore.YELLOW}Keyboard interrupt caught, exiting...")
+    RPC.clear()
+    RPC.close()
+    exit()
